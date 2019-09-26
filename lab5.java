00036867d6d59ee8c045606f7d688c99cdd479c6
Abed Islam <abed@Abeds-MacBook-Pro.local>
2019-09-26 01:27:44 -0400

    First commit

---
+++ lab5.java
@@ -0,0 +1,30 @@
+/*
+Create a class Circle with one instance variable of type double called radius. 
+Then define an appropriate constructor that takes an initial value for the radius, get and set methods for the radius, 
+and methods getArea and getPerimeter.Create a class RightTriangle with three instance variables of type double called base, height, 
+and hypotenuse. 
+
+Then define an appropriate constructor that takes initial values for the base and height and calculates the hypotenuse, 
+a single set method which takes new values for base and height and calculates the hypotenuse, get methods for all three instance variables, 
+and methods getArea and getPerimeter.Then create a test application which queries the user for the needed data to create two Circle objects 
+and two RightTriangle objects.
+
+ It will then display all information about the four objects. 
+ It should then display which Circle object has the largest area and which RightTriangle object has the largest area.
+*/
+
+ import java.util.Scanner;
+
+ public class Lab5{
+ 	public static void main(String[] args){
+
+ 	}
+ }
+
+ class Circle{
+
+ }
+
+ class RightTriangle{
+ 	
+ }
