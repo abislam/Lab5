f8b5e3c183b795560f76a0e1aaaaabccfb21c063
Abed Islam <abed@Abeds-MacBook-Pro.local>
2019-09-26 01:50:30 -0400

    Nothing done yet

--- lab5.java
+++ lab5.java
@@ -1,12 +1,14 @@
 /*
 Create a class Circle with one instance variable of type double called radius. 
 Then define an appropriate constructor that takes an initial value for the radius, get and set methods for the radius, 
-and methods getArea and getPerimeter.Create a class RightTriangle with three instance variables of type double called base, height, 
-and hypotenuse. 
+and methods getArea and getPerimeter.
 
+Create a class RightTriangle with three instance variables of type double called base, height, 
+and hypotenuse. 
 Then define an appropriate constructor that takes initial values for the base and height and calculates the hypotenuse, 
 a single set method which takes new values for base and height and calculates the hypotenuse, get methods for all three instance variables, 
-and methods getArea and getPerimeter.Then create a test application which queries the user for the needed data to create two Circle objects 
+and methods getArea and getPerimeter.
+Then create a test application which queries the user for the needed data to create two Circle objects 
 and two RightTriangle objects.
 
  It will then display all information about the four objects. 
@@ -22,9 +24,10 @@ and two RightTriangle objects.
  }
 
  class Circle{
+ 	double radius;
 
  }
 
  class RightTriangle{
- 	
+
  }
