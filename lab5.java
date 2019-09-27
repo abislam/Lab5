c82d90edc82e471d98f89dbf21f2cf75f8b2d1c5
Abed <aislam360@gmail.com>
2019-09-27 01:13:27 -0400

    RightTriangle class works. base height hypotenuse calculated successfully.

--- lab5.java
+++ lab5.java
@@ -3,11 +3,7 @@ Create a class Circle with one instance variable of type double called radius.
 Then define an appropriate constructor that takes an initial value for the radius, get and set methods for the radius, 
 and methods getArea and getPerimeter.(done)
 
-Create a class RightTriangle with three instance variables of type double called base, height, 
-and hypotenuse. 
-Then define an appropriate constructor that takes initial values for the base and height and calculates the hypotenuse, 
-a single set method which takes new values for base and height and calculates the hypotenuse, get methods for all three instance variables, 
-and methods getArea and getPerimeter.
+
 
 Then create a test application which queries the user for the needed data to create two Circle objects 
 and two RightTriangle objects.
@@ -36,8 +32,19 @@ It should then display which Circle object has the largest area and which RightT
  		System.out.println("circle2 area: " + circle2.getArea());
  		System.out.println("circle2 perimeter: " + circle2.getPerimeter());
 
+		System.out.println("//////////////////////////********************");
 
+ 		RightTriangle rt1 = new RightTriangle(22,31);
+ 		//System.out.println(rt1.getBase());
+ 		//System.out.println(rt1.getHeight());
+ 		//System.out.println(rt1.getHypotenuse());
 
+ 		System.out.println("//////////////////////////********************");
+
+ 		rt1.setValues(34.0,50.0);
+ 		//System.out.println(rt1.getBase());
+ 		//System.out.println(rt1.getHeight());
+ 		//System.out.println(rt1.getHypotenuse());
  	}
  }
 
@@ -69,9 +76,69 @@ It should then display which Circle object has the largest area and which RightT
  }
 
  
+/*
+Create a class RightTriangle with three instance variables of type double called base, height, 
+and hypotenuse. 
+Then define an appropriate constructor that takes initial values for the base and height and calculates the hypotenuse, 
+a single set method which takes new values for base and height and calculates the hypotenuse, get methods for all three instance variables, 
+and methods getArea and getPerimeter.
+*/
+
  class RightTriangle{
+ 	private double base, height, hypotenuse;
+
+ 	//getBase
+ 	public double getBase(){
+ 		return base;
+ 	}
+
+ 	//getHeight
+ 	public double getHeight(){
+ 		return height;
+ 	}
+
+ 	//getHypotenuse
+ 	public double getHypotenuse(){
+ 		hypotenuse = Math.sqrt(Math.pow(getBase(), 2) + Math.pow(getHeight(), 2));
+ 		return hypotenuse;
+ 	}
+
+ 	//setValues
+ 	public void setValues(double newBase, double newHeight){
+ 		this.base = newBase;
+ 		this.height = newHeight;
+ 		this.hypotenuse = getHypotenuse();
+ 		System.out.println("Hypotenuse: " + getHypotenuse());
+
+ 	}
+
+ 	public RightTriangle(double base, double height){
+ 		this.base = base;
+ 		this.height = height;
+ 		System.out.println("Hypotenuse: " + getHypotenuse());
+ 		
+ 	}
+
+
 
  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
